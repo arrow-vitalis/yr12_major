e2c3eddfaeeb2181a30e0e943fceebec641e3357
arrow vitalis <tvitalis@pymblelc.nsw.edu.au>
2022-04-07 10:21:26 +1000

    got nav bar working properly + form showing in console once complete

--- js/major.js
+++ js/major.js
@@ -6,36 +6,36 @@ var arrSize = ["small", "medium", "large"];
 
 var arrTempLevel = ["iced", "warm", "normal", "extra hot", "super-duper hot"];
 
-function loadCoffeeTypes(){
-    for (var i = 0; i<arrCoffeeTypes.length; i++){
-        var tempString = "<option value='" + arrCoffeeTypes[i] + "' > " + arrCoffeeTypes[i] + " </option>" ;
+function loadCoffeeTypes() {
+    for (var i = 0; i < arrCoffeeTypes.length; i++) {
+        var tempString = "<option value='" + arrCoffeeTypes[i] + "' > " + arrCoffeeTypes[i] + " </option>";
         $('#coffeeType').append(tempString);
     }
 }
 
 loadCoffeeTypes();
 
-function loadMilkTypes(){
-    for (var i = 0; i<arrMilkTypes.length; i++){
-        var tempString = "<option value='" + arrMilkTypes[i] + "' > " + arrMilkTypes[i] + " </option>" ;
+function loadMilkTypes() {
+    for (var i = 0; i < arrMilkTypes.length; i++) {
+        var tempString = "<option value='" + arrMilkTypes[i] + "' > " + arrMilkTypes[i] + " </option>";
         $('#milkType').append(tempString);
     }
 }
 
 loadMilkTypes();
 
-function loadSize(){
-    for (var i = 0; i<arrSize.length; i++){
-        var tempString = "<option value='" + arrSize[i] + "' > " + arrSize[i] + " </option>" ;
+function loadSize() {
+    for (var i = 0; i < arrSize.length; i++) {
+        var tempString = "<option value='" + arrSize[i] + "' > " + arrSize[i] + " </option>";
         $('#size').append(tempString);
     }
 }
 
 loadSize();
 
-function loadTempLevel(){
-    for (var i = 0; i<arrTempLevel.length; i++){
-        var tempString = "<option value='" + arrTempLevel[i] + "' > " + arrTempLevel[i] + " </option>" ;
+function loadTempLevel() {
+    for (var i = 0; i < arrTempLevel.length; i++) {
+        var tempString = "<option value='" + arrTempLevel[i] + "' > " + arrTempLevel[i] + " </option>";
         $('#tempLevel').append(tempString);
     }
 }
@@ -47,34 +47,48 @@ $("#viewMenu").hide();
 $("#orderHere").hide();
 $("#aboutUs").hide();
 
+function resetActiveNav() {
+    document.getElementById("homeTab").classList.remove("active")
+    document.getElementById("ourMenu").classList.remove("active")
+    document.getElementById("orderForm").classList.remove("active")
+    document.getElementById("aboutCafe").classList.remove("active")
+}
 
-$("#homeTab").click(function(){
+$("#homeTab").click(function () {
+    resetActiveNav();
+    document.getElementById("homeTab").classList.add("active")
     $("#viewMenu").hide();
     $("#orderHere").hide();
     $("#aboutUs").hide();
     $("#homePage").show();
-  });
+});
 
-$("#ourMenu").click(function(){
+$("#ourMenu").click(function () {
+    resetActiveNav();
+    document.getElementById("ourMenu").classList.add("active")
     $("#homePage").hide();
     $("#orderHere").hide();
     $("#aboutUs").hide();
     $("#viewMenu").show();
-  });  
+});
 
-$("#orderForm").click(function(){
+$("#orderForm").click(function () {
+    resetActiveNav();
+    document.getElementById("orderForm").classList.add("active")
     $("#viewMenu").hide();
     $("#homePage").hide();
     $("#aboutUs").hide();
     $("#orderHere").show();
-  });
-$("#aboutCafe").click(function(){
+});
+$("#aboutCafe").click(function () {
+    resetActiveNav();
+    document.getElementById("aboutCafe").classList.add("active")
     $("#viewMenu").hide();
     $("#orderHere").hide();
     $("#homePage").hide();
     $("#aboutUs").show();
-    
-  });
+
+});
 
 
 /*to complete order and send it to owner
@@ -83,7 +97,15 @@ When button(completeOrder) = clicked:
     console.log (all inputs) --> for now
 
 */
-$("#completeOrder").click(function(){
-    console.log("[" + "#customerName" + ", " + "#customerPhone" + ", " + "#coffeeType" + ", " + "#milkType" + ", " + " #size" + ", " + "#tempLevel" + ", " + "#decaf" +  ", " + "#sugars" + ", " + "#payment" +"]")
+$("#completeOrder").click(function () {
+    console.log("the customer name is: " + document.getElementById("customerName").value)
+    console.log("the customer phone number is: " + document.getElementById("customerPhone").value)
+    console.log("the customer name is: " + document.getElementById("customerName").value)
+    console.log("the customer name is: " + document.getElementById("customerName").value)
+    console.log("the customer name is: " + document.getElementById("customerName").value)
+    console.log("the customer name is: " + document.getElementById("customerName").value)
+    console.log("the customer name is: " + document.getElementById("customerName").value)
+    console.log("the customer name is: " + document.getElementById("customerName").value)
+    console.log("the customer name is: " + document.getElementById("customerName").value)
 })
 
